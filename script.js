22348a9df5efbbe23e8906be26c49167b93a1bdb
Bruno Sales Maia <145605639+bruno-s-m@users.noreply.github.com>
2024-09-14 16:50:17 -0300

    senha

--- script.js
+++ script.js
@@ -7,19 +7,14 @@ function senhanormal() {
       numero: numeroSenha,
       senha: `Senha ${numeroSenha} - ${dataHora}`
     };
-  
-    // Obter as senhas existentes ou criar um novo array
+
     let senhas = JSON.parse(localStorage.getItem('senhas')) || [];
-  
-    // Adicionar a nova senha ao array
+
     senhas.push(novaSenha);
   
-    // Atualizar o número da senha para a próxima
     localStorage.setItem('numSenha', parseInt(numeroSenha) + 1);
-  
-    // Armazenar as senhas atualizadas no localStorage
+
     localStorage.setItem('senhas', JSON.stringify(senhas));
   
-    // Exibir um alerta com a nova senha
     alert('Nova senha gerada: ' + novaSenha.senha);
   }
